59512bfa5225900748a7d596ecd234809e6e35a4
lukacafuta <luka.cafuta@gmail.com>
2024-06-22 17:58:26 +0200

    added serializer, views (get/post), urls (comment, mainProject)

--- comment/urls.py
+++ comment/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from comment.views import ListCreateCommentsView
+
 urlpatterns = [
-    path('', )
+    path('<int:post_id>/', ListCreateCommentsView.as_view())
 ]
--- mainProject/urls.py
+++ mainProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework_simplejwt import views as jwt_views  # JWT
 
@@ -27,5 +27,6 @@ urlpatterns = [
     path('api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/token/verify/',  jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
     # ... end JWT
+    path('api/social/comments/', include('comment.urls')),
 ]
 
