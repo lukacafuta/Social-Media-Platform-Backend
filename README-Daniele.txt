c3209353e11991640079f94d0b2a863a6ab303f6
Daniele Scopece <daniele.scopece@gmail.com>
2024-06-22 12:40:15 +0200

    240622.01.daniele JWT works

---
+++ README-Daniele.txt
@@ -0,0 +1,7 @@
+
+
+# 2024-06-22 Sat
+-) 11:00 -> 11:30 : JWT in branch daniele_jwt
+-) 11:30 -> 11:52 : renew PyCharm
+-) 11:55 -> 12:20 : set up Postman to test JWT all cases and set up variables
+-) 12:40 -> : push branch and test to merge into master locally first
--- mainProject/urls.py
+++ mainProject/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework_simplejwt import views as jwt_views  # JWT
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # +++ Start JWT
+    path('api/auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify/',  jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
+    # ... end JWT
 ]
