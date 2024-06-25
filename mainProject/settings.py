c4f8b1d7f8e51f2d826dade9b1029b3dd0d4f65b
lukacafuta <luka.cafuta@gmail.com>
2024-06-25 19:49:19 +0200

    before deployment

--- mainProject/settings.py
+++ mainProject/settings.py
@@ -61,6 +61,7 @@ AUTH_USER_MODEL = 'customUser.CustomUser' # name of App. name of model
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
