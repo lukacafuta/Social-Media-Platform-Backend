ea6ff94e4e284ac276edb526b6736e64ab33abdf
lukacafuta <luka.cafuta@gmail.com>
2024-06-27 00:19:29 +0200

    out-commented last changes in settings.py and deleted DISABLE_COLLECTSTATIC variable from heroku

--- mainProject/settings.py
+++ mainProject/settings.py
@@ -156,11 +156,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+#
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
